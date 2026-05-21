986a2f285463432e18569acb2c0e684f05f71023
taganiimmo-maker <taganiimmo@gmail.com>
2026-05-21 04:30:15 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TaganiImmo from '../TaganiImmo_v3.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <TaganiImmo />
+        </React.StrictMode>
+        )
